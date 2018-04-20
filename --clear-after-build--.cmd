553ff4e9c0349214ee55bae3eae6491dfb068b93
zamgi <denisov72@mail.ru>
2018-04-20 15:14:41 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,14 @@
+rem del "*.suo" /S/Q/F/A
+del "*.csproj.user" /S/Q/F/A
+rd ".vs\TextSegmenter\v15\Server" /S/Q
+del "ts.web.demo\bin\*.pdb" /Q
+rd "ts.web.demo\obj" /S/Q
+rd "ts.web.demo\(logs)" /S/Q
+rd "ts.TestApp\bin" /S/Q
+rd "ts.TestApp\obj" /S/Q
+rd "TextSegmenter\bin" /S/Q
+rd "TextSegmenter\obj" /S/Q
+rd "model-builder\ts.model-builder.console\bin" /S/Q
+rd "model-builder\ts.model-builder.console\obj" /S/Q
+rd "model-builder\ts.model-converter-txt-2-bin.console\bin" /S/Q
+rd "model-builder\ts.model-converter-txt-2-bin.console\obj" /S/Q
