4486f26d4469e84df820d0e389a7044a7d2915ea
zamgi <denisov72@mail.ru>
2020-11-26 23:14:40 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,7 +1,7 @@
 rem del "*.suo" /S/Q/F/A
 del "*.csproj.user" /S/Q/F/A
 rd ".vs\TextSegmenter\v15\Server" /S/Q
-del "ts.web.demo\bin\*.pdb" /Q
+rd "ts.web.demo\bin" /S/Q
 rd "ts.web.demo\obj" /S/Q
 rd "ts.web.demo\(logs)" /S/Q
 rd "ts.TestApp\bin" /S/Q
