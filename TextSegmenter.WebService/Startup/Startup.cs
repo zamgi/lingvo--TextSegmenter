f620a19507d97c1f908ee15a25454b4de1e7dea9
zamgi <denisov72@mail.ru>
2022-10-17 16:11:20 +0300

    ...

--- TextSegmenter.WebService/Startup/Startup.cs
+++ TextSegmenter.WebService/Startup/Startup.cs
@@ -26,7 +26,7 @@ namespace lingvo.ts.webService
     {
         public const string INDEX_PAGE_PATH = "/index.html";
 
-        public void ConfigureServices( IServiceCollection services )
+        public static void ConfigureServices( IServiceCollection services )
         {
             services.AddControllers().AddJsonOptions( options =>
             {
@@ -45,8 +45,7 @@ namespace lingvo.ts.webService
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
+        public static void Configure( IApplicationBuilder app, IWebHostEnvironment env )
         {
             if ( env.IsDevelopment() )
             {
@@ -76,8 +75,8 @@ namespace lingvo.ts.webService
                     ctx.Response.Redirect( INDEX_PAGE_PATH );
                 }
             });
-            //-------------------------------------------------------------//
 #if DEBUG
+            //-------------------------------------------------------------//
             OpenBrowserIfRunAsConsole( app );
 #endif
         }
